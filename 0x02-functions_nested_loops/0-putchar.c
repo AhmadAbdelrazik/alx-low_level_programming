b278b8bccff559ab1188d6d2ceaa988caa18c9ca
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-06-23 00:28:05 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,14 @@
 #include "main.h"
-
+/**
+ * main - Entry Function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char word[9] = "_putchar";
 	int i = 0;
-	
+
 	while (i < 8)
 	{
 		_putchar(word[i]);
