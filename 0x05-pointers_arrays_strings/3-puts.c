97e27fd0809b787a2c6ca175ef5dc0d0c561e5c7
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-06-26 22:35:24 +0300

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - print a given string.
+ * @str: the string pointer.
+ *
+ * Return: Void.
+ */
+void _puts(char *str)
+{
+	int length = 0;
+	
+	while (*(str + length) != '\0')
+	{
+		_putchar(*(str + length));
+	}
+	_putchar('\n');
+}
