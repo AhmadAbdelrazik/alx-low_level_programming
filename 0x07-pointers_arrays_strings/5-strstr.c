79851e3542b1dc15f5654ed72a6d72ccd511e52d
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-04 23:25:58 +0300

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,11 +15,13 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-	for (j = 0; needle[j] != '\0'; j++)
+	for (j = 0; needle[j] != '\0';)
 	{
 	while (needle[j] == '\0' || needle[j] == haystack[i + j])
 	{
-	if (needle[j] == '\0')
+	if (needle[j] != '\0')
+		j++;
+	else
 		return (haystack + i);
 	}
 		break;
