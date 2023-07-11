6ffc4ba3d5021cf85edab9cbc2b9ece86e79f2f1
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-12 01:37:42 +0300

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * str_concat - concatenate two strings.
+ * @s1: the first string.
+ * @s2: the second string.
+ *
+ * Return: return pointer to the newly allocated memory
+ * return NULL at failure.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int count1 = 0, count2 = 0;
+	char *ptr;
+
+	while (s1[count1] != '\0')
+	{
+		count1++;
+	}
+	while (s2[count2] != '\0')
+	{
+		count2++;
+	}
+	ptr = (char *)malloc(count1 + count2 + 1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	do {
+		ptr[count1 + count2] = s2[count2];
+	} while (count2--);
+	while (count1--)
+	{
+		ptr[count1] = s1[count1];
+	}
+	return (ptr);
+}
