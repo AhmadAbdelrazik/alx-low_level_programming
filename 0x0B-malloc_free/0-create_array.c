349aa8951c3dadcde0675eecf4540a14476bc57a
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-12 01:17:26 +0300

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *ptr = malloc(size);
 
-	if (*ptr == NULL || size == 0)
+	if (ptr == NULL || size == 0)
 	{
 		return (NULL);
 	}
