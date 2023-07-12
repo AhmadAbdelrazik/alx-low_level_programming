3c83c530ea73b2f8400904c32f38d673d9c6e8e2
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-13 02:02:30 +0300

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main entry.
+ * @argc: argument count
+ * @argv: arguments.
+ *
+ * Return: 0 at success.
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+	if (argv[i][j] > '9' || argv[i][j] < '0')
+	{
+		printf("Error\n");
+		return (1);
+	}
+	}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
