47bac3981da239773c0fbd9124ecb90326ecf49f
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-15 01:15:39 +0300

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,13 +8,13 @@
  */
 int *array_range(int min, int max)
 {
-	unsigned int i;
-	int *p;
+	int *p, i, size;
 
 	if (min > max)
 		return (NULL);
-	p = malloc(max - min + 1);
-	for (i = 0; i < max - min + 1; i++)
+	size = max - min + 1;
+	p = malloc(size * sizeof(int));
+	for (i = 0; i < size; i++)
 	{
 		p[i] = min + i;
 	}
