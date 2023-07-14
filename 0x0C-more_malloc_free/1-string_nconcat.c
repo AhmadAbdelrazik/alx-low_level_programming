5db54da7dd650f70c6a0ec7594064a9dfdfdd4ce
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-14 20:01:14 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (size2 = 0; s2[size2] != '\0'; size2++)
 	;
 	if (size2 < n)
-		n - size2;
+	{
+		n = size2;
+	}
 	ptr = malloc(size1 + n);
 	if (ptr == NULL)
 	{
