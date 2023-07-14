22e3b521ae7926f5a96ad1deb51bc2613bbcf8de
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-14 22:56:37 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int size1, size2, i,j;
+	unsigned int size1, size2, i;
 	char *ptr;
 
 	/* Dealing with strings if NULL is passed */
