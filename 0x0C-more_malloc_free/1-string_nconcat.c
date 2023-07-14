9ff7e38497c15d3763cc1f7e20de245719ad321f
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-14 22:42:14 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,6 +14,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size1, size2, i;
 	char *ptr;
 
+	/* Dealing with strings if NULL is passed */
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -22,16 +23,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
+	/* Calculate the length of the strings */
 	for (size1 = 0; s1[size1] != '\0'; size1++)
 	;
 	for (size2 = 0; s2[size2] != '\0'; size2++)
 	;
+	/* Allocating memory*/
 	ptr = malloc(size1 + n + 1);
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i <= size1 + n; i++)
+	/* Copying the strings*/
+	for (i = 0; i <= size1 + (n > size2 ? size2 : n); i++)
 	{
 	if (i < size1)
 		ptr[i] = s1[i];
