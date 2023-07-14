231cd0a1d67c5a1130a0aff342adb67645c0ba20
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-14 22:05:58 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < size1 + n; i++)
+	for (i = 0; i <= size1 + n; i++)
 	{
 	if (i < size1)
 		ptr[i] = s1[i];
