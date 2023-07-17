87e4c43d8b40ce0883a3e712e3169cbfa1d7a2d2
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-17 17:46:47 +0300

    1-init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,21 +1,23 @@
 #include "dog.h"
 
 /**
- * init_dog - initialize a variable of type dog.
- * @d: the structure dog.
- * @name: the dog's name.
- * @age: the dog's age.
- * @owner: the dog's owner.
+ * init_dog - initialize dog struct
  *
- * Return: Void.
- */
+ * @d: input pointer to struct dog
+ * @name: input for dog name
+ * @age: input for dog age
+ * @owner: input for dog owner
+ *
+ * Return: nothing
+*/
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
+	if (d)
 	{
-		d = malloc(sizeof(struct dog));
+		/*d->name = name;*/
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
 	}
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
 }
---
+++ 0x0E-structures_typedef/1-init_dogzzzz.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type dog.
+ * @d: the structure dog.
+ * @name: the dog's name.
+ * @age: the dog's age.
+ * @owner: the dog's owner.
+ *
+ * Return: Void.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+	}
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
