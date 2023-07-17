fa1a3b30c0d9f660285c1aeb7d2292b1978fc0c0
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-17 17:34:17 +0300

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+
 /**
  * init_dog - initialize a variable of type dog.
  * @d: the structure dog.
@@ -12,8 +13,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
