a8dfdab7bf87c8f0d0e41f30a01082af15a48bd6
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-17 22:58:56 +0300

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,42 +14,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *cname, *cowner;
 	int len_name = 0, len_owner = 0, i = 0;
 
-	/* Early Terminate Conditions */
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
-	dog = imalloc(sizeof(dog_t));
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-
-	/* Length of name and owner */
 	for (; name[len_name] != '\0'; len_name++)
 	;
 	for (; owner[len_owner] != '\0'; len_owner++)
 	;
-
-	/* Allocating memory for cname and cowner */
 	cname = malloc(len_name + 1);
 	if (cname == NULL)
 		return (NULL);
-
 	cowner = malloc(len_owner + 1);
 	if (cowner == NULL)
 		return (NULL);
-
-	/* Copying the strings. */
 	for (i = 0; i < len_name; i++)
 		cname[i] = name[i];
 	cname[i] = '\0';
-
 	for (i = 0; i < len_owner; i++)
 		cowner[i] = owner[i];
 	cowner[i] = '\0';
-
-	/* completing the structure*/
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
-	
 	return (dog);
 }
