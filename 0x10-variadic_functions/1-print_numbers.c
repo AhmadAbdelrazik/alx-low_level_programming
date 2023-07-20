5016dc72929e55bd15da6fcd27a8af98825a4c49
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-07-20 23:36:55 +0300

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,6 +13,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	int x;
 
+	if (n == 0)
+		return;
 	va_start(args, n);
 	if (separator == NULL)
 		separator = "";
