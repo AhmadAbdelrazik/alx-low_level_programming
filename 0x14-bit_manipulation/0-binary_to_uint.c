d2ad2c3a8944a1db32aaf9b99768aa6164e6166b
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-08-03 22:24:30 +0300

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,15 +10,16 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int a = 0, i = 0;
 
-	if (b == 0)
+	if (*b == '\0')
 		return (0);
 	while (b[i])
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
+		a = a << 1;
 		a += b[i] - '0';
-		a << 1;
+
 		i++;
 	}
 	return (a);
