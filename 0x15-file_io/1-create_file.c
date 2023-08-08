dc2fe4784405f8ddcfe6eec065d6cdaf5b4a66ee
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-08-08 20:54:24 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-	if (text_count)
+	if (text_content)
 	{
 		while (text_count[words] != '\0')
 			words++;
