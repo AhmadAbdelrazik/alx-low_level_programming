c08a184d9561bb433979b2aea3b7a4ac8e755d62
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-08-08 21:00:48 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC , S_IRUSR | S_IWUSR);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
 
@@ -26,8 +26,7 @@ int create_file(const char *filename, char *text_content)
 		if (write_status)
 			return (-1);
 	}
-	else
-		return (-1);
+
 	close(file);
 	return (1);
 }
