cdce661b7004ea78dc70d8a05a459f07f302c090
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2023-08-08 20:16:27 +0300

    0-read_textfile.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#define HEADER_H
+#ifndef HEADER_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar.c(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* HEADER_H */
